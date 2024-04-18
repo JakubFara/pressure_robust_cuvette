8d777d8d591ce3c341ebc3972dbbffa9e9c3c689
Jakub Fara <jakubfara77@gmail.com>
2024-04-18 17:23:28 +0200

    cuvette

--- cuvette.py
+++ cuvette.py
@@ -6,6 +6,6 @@ mesh = fd.Mesh("square_with_hole.msh")
 k = 2
 V = fd.FunctionSpace(mesh, 'CG', k)
 
-x = fdSpatialCoordinate(mesh)
+x = fd.SpatialCoordinate(mesh)
 expr = x[0] / fd.sqrt(x[0]**2 + x[1]**2)
 bc_bot_v = fd.DirichletBC(V.sub(0).sub(0), expr, 3)
