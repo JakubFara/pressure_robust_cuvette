d135c588ee49bc712985a50b6ad4f5217750161d
Jakub Fara <jakubfara77@gmail.com>
2024-04-18 17:22:43 +0200

    cuvette

--- cuvette.py
+++ cuvette.py
@@ -4,8 +4,8 @@ import firedrake as fd
 mesh = fd.Mesh("square_with_hole.msh")
 
 k = 2
-V = FunctionSpace(m, 'CG', k)
+V = fd.FunctionSpace(m, 'CG', k)
 
-x = SpatialCoordinate(mesh)
-expr = x[0] / df.sqrt(x[0]**2 + x[1]**2)
+x = fdSpatialCoordinate(mesh)
+expr = x[0] / fd.sqrt(x[0]**2 + x[1]**2)
 bc_bot_v = fd.DirichletBC(V.sub(0).sub(0), expr, 3)
