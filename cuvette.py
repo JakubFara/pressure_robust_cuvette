959df927128833287c9fc65d5d8c8cab4a806f75
Jakub Fara <jakubfara77@gmail.com>
2024-04-18 17:29:50 +0200

    cuvette

--- cuvette.py
+++ cuvette.py
@@ -7,5 +7,30 @@ k = 2
 V = fd.FunctionSpace(mesh, 'CG', k)
 
 x = fd.SpatialCoordinate(mesh)
-expr = x[0] / fd.sqrt(x[0]**2 + x[1]**2)
-bc_bot_v = fd.DirichletBC(V.sub(0).sub(0), expr, 3)
+expr_x_outer = x[0] / fd.sqrt(x[0]**2 + x[1]**2)
+expr_y_outer = x[1] / fd.sqrt(x[0]**2 + x[1]**2)
+expr_x_inner = 0.5 * x[0] / fd.sqrt(x[0]**2 + x[1]**2)
+expr_y_inner = 0.5 * x[1] / fd.sqrt(x[0]**2 + x[1]**2)
+
+bc_x_outer = fd.DirichletBC(V.sub(0).sub(0), expr_x_outer, 1)
+bc_y_outer = fd.DirichletBC(V.sub(0).sub(1), expr_y_outer, 1)
+bc_x_inner = fd.DirichletBC(V.sub(0).sub(0), expr_x_inner, 2)
+bc_y_inner = fd.DirichletBC(V.sub(0).sub(1), expr_y_inner, 2)
+
+bcs = [
+    bc_x_outer,
+    bc_y_outer,
+    bc_x_inner,
+    bc_y_inner,
+]
+
+dx = fd.dx(degree=4)
+
+phi_u = fd.TestFunction(V)
+u = fd.TrialFunction(V)
+
+form = fd.inner(fd.grad(u), fd.grad(phi_u)) * dx
+
+J = fd.derivative(form, u)
+
+problem = fd.NonlinearVariationalProblem(form, w, bcs=bcs, J=J)
