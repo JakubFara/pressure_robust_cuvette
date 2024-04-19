816717685f511236de8d1e09cd96cec59f0d10b7
Jakub Fara <jakubfara77@gmail.com>
2024-04-19 14:53:26 +0200

    mesh

--- cuvette.py
+++ cuvette.py
@@ -12,7 +12,7 @@ x, y = fd.SpatialCoordinate(mesh)
 expr_x_outer = x / fd.sqrt(x**2 + y**2) - x
 expr_y_outer = y / fd.sqrt(x**2 + y**2) - y
 expr_x_inner = 0.5 * x / fd.sqrt(x**2 + y**2) - x
-expr_y_inner = 0.5 * y / fd.sqrt(x**2 + y**2) - y
+expr_y_inner = 0.5  # * y / fd.sqrt(x**2 + y**2) - y
 
 bc_x_outer = fd.DirichletBC(V.sub(0), expr_x_outer, (11, 12, 13, 14))
 bc_y_outer = fd.DirichletBC(V.sub(1), expr_y_outer, (11, 12, 13, 14))
