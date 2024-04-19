d7f4fc0e4498a54d49636603bb402537ca979f48
Jakub Fara <jakubfara77@gmail.com>
2024-04-19 15:17:14 +0200

    mesh

--- cuvette.py
+++ cuvette.py
@@ -115,11 +115,11 @@ T = - p * I + 2.0 * mu * D
 # Data Forces
 force = fd.Constant((0, 0))
 
-Eq1 = fd.div(v_hat) * p_ * fd.dx
+Eq1 = fd.div(v_hat) * phi_p * fd.dx
 
 Eq2 = (
-    + J * fd.inner(T * inv_F.T, fd.grad(v_)) * fd.dx
-    - J * rho * fd.inner(force, v_) * fd.dx
+    + J * fd.inner(T * inv_F.T, fd.grad(phi_v)) * fd.dx
+    - J * rho * fd.inner(force, phi_v) * fd.dx
 )
 
 Eq = Eq1 + Eq2
